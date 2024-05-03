66ce4b48145372f73765f77d8b8cd1744e2068f9
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2024-05-03 08:42:38 +0545

    feat: use case of slugs

---
+++ src/routes/houses/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {page} from "$app/stores"
+    const slugArray = $page.params.slug.split("/");
+    
+</script>
+
+{#if slugArray.length === 1 && slugArray[0] === ""}
+<h1>List of all houses</h1>
+{:else if slugArray.length === 1}
+<h1>List of houses in {slugArray[0]} area.</h1>
+{:else if slugArray.length === 2}
+<h1>List of houses in {slugArray[0]} area and under Rs. {slugArray[1]}.</h1>
+{/if}
