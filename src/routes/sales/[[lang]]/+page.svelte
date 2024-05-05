f9e2d2c887b07863259bb33e4f078f83a1f55f06
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2024-05-05 06:49:49 +0545

    feat: optional parameter while routing

---
+++ src/routes/sales/[[lang]]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+    import { page } from "$app/stores"
+
+    type Language = "en" | "es" | "fr";
+
+    const language = {
+        en: {
+            title: "What is your role?",
+            description: "Select the role that best describes you."
+        },
+        es: {
+            title: "¿Cuál es tu rol?",
+            description: "Selecciona el rol que mejor te describe."
+        },
+        fr: {
+            title: "Quel est votre rôle?",
+            description: "Sélectionnez le rôle qui vous décrit le mieux."
+        },
+    }
+
+    const {lang = "en"} = $page.params as {lang : Language};
+
+    const greetings = language[lang as Language];
+    
+</script>
+
+<h1>{greetings.title}</h1>
+<p>{greetings.description}</p>
