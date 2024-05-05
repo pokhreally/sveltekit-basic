d3479415e2b30872b5efd8f7a5c14d87f04304e4
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2024-05-05 07:23:05 +0545

    feat: layout and layout groups

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to the Coding world</h1>
+<slot />
---
+++ src/routes/products/[productId=integer]/+layout.svelte
@@ -0,0 +1,8 @@
+<h1>Featured Products</h1>
+<ul>
+    <li><a href="/products/7">Product 7</a></li>
+    <li><a href="/products/8">Product 8</a></li>
+    <li><a href="/products/9">Product 9</a></li>
+    <li><a href="/products/10">Product 10</a></li>
+</ul>
+<slot />
